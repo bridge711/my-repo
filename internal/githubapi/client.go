9a7774c0a449909e763e395e24e017eb0347ebbc
bridge711 <uc84@yahoo.ne.jp>
2025-11-13 21:32:33 +0900

    Update client.go

--- internal/githubapi/client.go
+++ internal/githubapi/client.go
@@ -1,47 +1,47 @@
 package githubapi
 
 import (
-	"context"
-	"fmt"
-	"time"
+    "context"
+    "fmt"
+    "time"
 
-	"github.com/go-resty/resty/v2"
+    "github.com/go-resty/resty/v2"
 )
 
 type Client struct {
-	r *resty.Client
+    r *resty.Client
 }
 
-// New は Github API用クライアントを作成します。
+// New は GitHub API 用クライアントを作成します。
 // baseURL は通常 "https://api.github.com" を指定します。
 func New(baseURL string) *Client {
-	c := resty.New().
-		SetBaseURL(baseURL).
-		SetTimeout(5*time.Second).
-		SetHeader("Accept", "application/vnd.github+json")
+    c := resty.New().
+        SetBaseURL(baseURL).
+        SetTimeout(5 * time.Second).
+        SetHeader("Accept", "application/vnd.github+json")
 
-	return &Client{r: c}
+    return &Client{r: c}
 }
 
 // User は最小限のフィールドだけ持つサンプル構造体です。
 type User struct {
-	Login string `json:"login"`
-	ID    int64  `json:"id`
-	Name  string `json:"name"`
+    Login string `json:"login"`
+    ID    int64  `json:"id"`
+    Name  string `json:"name"`
 }
 
+// GetUser は /users/{username} を GET して JSON を構造体にマッピングします。
 func (c *Client) GetUser(ctx context.Context, username string) (*User, error) {
-	resp, err := c.r.R().
-		SetContext(ctx).
-		SetResult(&User{}).
-		Get("/users/" + username)
+    resp, err := c.r.R().
+        SetContext(ctx).
+        SetResult(&User{}).
+        Get("/users/" + username)
 
-	if err != nil {
-		return nil, err
-	}
-	if resp.IsError() {
-		return nil, fmt.Errorf("github api error: status=%d", resp.StatusCode())
-	}
-	return resp.Request().(*User), nil
-
+    if err != nil {
+        return nil, err
+    }
+    if resp.IsError() {
+        return nil, fmt.Errorf("github api error: status=%d", resp.StatusCode())
+    }
+    return resp.Result().(*User), nil
 }
